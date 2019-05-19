9efc27d0df399ffd14bad1533627f66afe5284e8
Alexander B. Waldner <abwaldner@yandex.ru>
2019-05-20 02:16:54 +0300

    fix for sandboxed install

--- mkTextDomains.sh
+++ mkTextDomains.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-  for F in "${1}"/*.mo ; do
-    D=${F%.mo}/LC_MESSAGES/ ; mkdir -p "${D}" ; mv -f "${F}" "${D}/${2}.mo"
+  for F in "${INSTALL_ROOT}/${1}/"*.mo ; do
+    if [ -f "${F}" ] ; then
+      D=${F%.mo}/LC_MESSAGES/ ; mkdir -p "${D}" ; mv -f "${F}" "${D}/${2}.mo"
+    fi
   done
 
 #eof
